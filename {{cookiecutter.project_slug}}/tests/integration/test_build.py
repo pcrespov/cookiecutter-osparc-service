f842e0b7717b1a1748aa860d4009cc4f0b7f198b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-03-07 15:46:24 +0100

    fixed integration tests

--- {{cookiecutter.project_slug}}/tests/integration/test_build.py
+++ {{cookiecutter.project_slug}}/tests/integration/test_build.py
@@ -11,6 +11,7 @@ import shutil
 import subprocess
 import urllib.request
 from pathlib import Path
+from typing import Dict
 
 import docker
 import jsonschema
@@ -25,9 +26,9 @@ def docker_client() -> docker.DockerClient:
 @pytest.fixture
 def docker_image_key(docker_client) -> str:
     image_key = "simcore/services/{%- if cookiecutter.project_type == 'computational' -%}comp{%- elif cookiecutter.project_type == 'dynamic' -%}dynamic{%- endif -%}/{{ cookiecutter.project_name.lower().replace(' ', '-') }}:latest"
-    docker_images = [image for image in docker_client.images.list() if image_key in image.tags]
+    docker_images = [image for image in docker_client.images.list() if any(image_key in tag for tag in image.tags)]
     assert len(docker_images) == 1
-    assert image_key in docker_images[0].tags
+    assert image_key in docker_images[0].tags[0]
     return docker_images[0].tags[0]
 
 @pytest.fixture
@@ -42,27 +43,35 @@ def _download_url(url, file: Path):
         shutil.copyfileobj(response, out_file)
     assert file.exists()
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def osparc_service_labels_jsonschema(tmp_path) -> Dict:
-    url = "https://github.com/ITISFoundation/osparc-simcore/blob/master/api/specs/shared/schemas/node-meta-v0.0.1.json"
+    url = "https://raw.githubusercontent.com/ITISFoundation/osparc-simcore/master/api/specs/shared/schemas/node-meta-v0.0.1.json"
     file_name = tmp_path / "service_label.json"
     _download_url(url, file_name)
     with file_name.open() as fp:
         json_schema = json.load(fp)
         return json_schema
 
-def _convert_to_simcore_labels(value: str):
-    simcore_label = json.loads(value)
-    key = list(simcore_label.keys())[0]
-    value = simcore_label[key]
-    return key,value
+def _convert_to_simcore_labels(image_labels: Dict) -> Dict:
+    io_simcore_labels = {}
+    for key, value in image_labels.items():
+        if str(key).startswith("io.simcore."):
+            simcore_label = json.loads(value)
+            simcore_keys = list(simcore_label.keys())
+            assert len(simcore_keys) == 1
+            simcore_key = simcore_keys[0]
+            simcore_value = simcore_label[simcore_key]
+            io_simcore_labels[simcore_key] = simcore_value
+    assert len(io_simcore_labels) > 0
+    return io_simcore_labels
 
-def test_docker_io_simcore_labels_against_files(docker_image: docker.models.images.Image, docker_dir):
+
+def test_docker_io_simcore_labels_against_files(docker_image: docker.models.images.Image, docker_dir):    
     image_labels = docker_image.labels
-    io_simcore_labels = {_convert_to_simcore_labels(value) for key, value in image_labels.items() if str(key).startswith("io.simcore.")}
-
-    for key, value in io_simcore_labels:
-        file_name = Path(docker_dir / "{}.json".format(key))
+    io_simcore_labels = _convert_to_simcore_labels(image_labels)
+    # check files are identical
+    for key, value in io_simcore_labels.items():
+        file_name = Path(docker_dir / "labels" / "{}.json".format(key))
         assert file_name.exists()
         with file_name.open() as fp:
             label_dict = json.load(fp)
@@ -73,8 +82,7 @@ def test_docker_io_simcore_labels_against_files(docker_image: docker.models.imag
 def test_validate_docker_io_simcore_labels(docker_image: docker.models.images.Image, osparc_service_labels_jsonschema: Dict):
     image_labels = docker_image.labels
     # get io labels
-    io_simcore_labels = {_convert_to_simcore_labels(value) for key, value in image_labels.items() if str(key).startswith("io.simcore.")}
-    assert len(io_simcore_labels) > 0
+    io_simcore_labels = _convert_to_simcore_labels(image_labels)
     # validate schema
     try:
         jsonschema.validate(io_simcore_labels, osparc_service_labels_jsonschema)
