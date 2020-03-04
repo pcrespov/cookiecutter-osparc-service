f21d81e07f6030b13214cbd8b61bb880ec8931e4
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-04 17:23:16 +0100

    improve execution file

--- {{cookiecutter.project_slug}}/service.cli/execute.sh
+++ {{cookiecutter.project_slug}}/service.cli/execute.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
-cd "$(dirname "$0")"
-
 # set sh strict mode
 set -o errexit
 set -o nounset
 IFS=$(printf '\n\t')
 
+cd /home/scu/{{ cookiecutter.project_package_name }}
+
+echo "starting service as"
+echo   User    : "$(id "$(whoami)")"
+echo   Workdir : "$(pwd)"
+echo "..."
+echo
 # ----------------------------------------------------------------
 # This script shall be modified according to the needs in order to run the service
 # The inputs defined in ${INPUT_FOLDER}/inputs.json are available as env variables by their key in capital letters
 # For example: input_1 -> $INPUT_1
-# call stack:
-# run -> execute.sh
 
-cd /home/scu/{{ cookiecutter.project_package_name }}
 # put the code to execute the service here
-# and default all outputs to $LOG_FILE
-# For example: java someJavaClass >> $LOG_FILE
-ls -al >> "$LOG_FILE"
+# For example:
+env
+ls -al "${INPUT_FOLDER}"
 
 # then retrieve the output and move it to the $OUTPUT_FOLDER
 # as defined in the output labels
