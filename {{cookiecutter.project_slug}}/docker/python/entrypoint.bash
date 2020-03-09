b358a118ce4ff110f30f8aad26293e34bb06586e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-09 22:51:20 +0100

    added entrypoint in bash

--- {{cookiecutter.project_slug}}/docker/python/entrypoint.bash
+++ {{cookiecutter.project_slug}}/docker/python/entrypoint.bash
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 set -o errexit
 set -o nounset
+set -o pipefail
 
-IFS=$(printf '\n\t')
+IFS=$'\n\t'
 # This entrypoint script:
 #
 # - Executes *inside* of the container upon start as --user [default root]
@@ -33,13 +34,13 @@ then
 fi
 
 echo "setting correct user id/group id..."
-USERID=$(stat -c %u "${INPUT_FOLDER}")
-GROUPID=$(stat -c %g "${INPUT_FOLDER}")
-GROUPNAME=$(getent group "${GROUPID}" | cut -d: -f1)
+USERID=$(stat --format=%u "${INPUT_FOLDER}")
+GROUPID=$(stat --format=%g "${INPUT_FOLDER}")
+GROUPNAME=$(getent group "${GROUPID}" | cut --delimiter=: --fields=1)
 if [ "$USERID" -eq 0 ]
 then
     echo "Warning: Folder mounted owned by root user... adding $SC_USER_NAME to root..."
-    addgroup "$SC_USER_NAME" root
+    adduser "$SC_USER_NAME" root
 else
     echo "Folder mounted owned by user $USERID:$GROUPID-'$GROUPNAME'..."
     # take host's credentials in $SC_USER_NAME
@@ -47,23 +48,23 @@ else
     then
         echo "Creating new group my$SC_USER_NAME"
         GROUPNAME=my$SC_USER_NAME
-        addgroup -g "$GROUPID" "$GROUPNAME"
+        addgroup --gid "$GROUPID" "$GROUPNAME"
         # change group property of files already around
-        find / -group "$SC_USER_ID" -exec chgrp -h "$GROUPNAME" {} \;
+        find / -path /proc -prune -group "$SC_USER_ID" -exec chgrp --no-dereference "$GROUPNAME" {} \;
     else
         echo "adding $SC_USER_NAME to group $GROUPNAME..."
-        addgroup "$SC_USER_NAME" "$GROUPNAME"
+        adduser "$SC_USER_NAME" "$GROUPNAME"
     fi
 
     echo "changing $SC_USER_NAME $SC_USER_ID:$SC_USER_ID to $USERID:$GROUPID"
     deluser "$SC_USER_NAME" > /dev/null 2>&1
     if [ "$SC_USER_NAME" = "$GROUPNAME" ]
     then
-        addgroup -g "$GROUPID" "$GROUPNAME"
+        addgroup --gid "$GROUPID" "$GROUPNAME"
     fi
-    adduser -u "$USERID" -G "$GROUPNAME" -D -s /bin/sh "$SC_USER_NAME"
+    adduser --disabled-password --gecos "" --uid "$USERID" --gid "$GROUPID" --shell /bin/bash "$SC_USER_NAME" --no-create-home
     # change user property of files already around
-    find / -user "$SC_USER_ID" -exec chown -h "$SC_USER_NAME" {} \;
+    find / -user "$SC_USER_ID" -exec chown --no-dereference "$SC_USER_NAME" {} \;
 fi
 
 echo "Starting $* ..."
@@ -72,4 +73,4 @@ echo "  local dir : $(ls -al)"
 echo "  input dir : $(ls -al "${INPUT_FOLDER}")"
 echo "  output dir : $(ls -al "${OUTPUT_FOLDER}")"
 
-su-exec "$SC_USER_NAME" "$@"
+su --preserve-environment --command "export PATH=${PATH}:/home/$SC_USER_NAME/service.cli; $*" "$SC_USER_NAME"
