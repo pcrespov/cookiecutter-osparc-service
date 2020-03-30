39b377eb8824cf8b5c98664762531195345609e7
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-30 14:13:58 +0200

    test all flavors

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -1,12 +1,21 @@
 #pylint: disable=W0621
 
+import json
 import logging
 import os
 import subprocess
+import sys
 from pathlib import Path
+from typing import Dict
+
 import pytest
 
+# current directory
+current_dir = Path(sys.argv[0] if __name__ ==
+                   "__main__" else __file__).resolve().parent
+
 logger = logging.getLogger(__name__)
+
 
 def test_project_tree(cookies):
     result = cookies.bake(extra_context={'project_slug': 'test_project'})
@@ -15,21 +24,33 @@ def test_project_tree(cookies):
     assert result.project.basename == 'test_project'
 
 
-@pytest.fixture
-def baked_project(cookies):
-    return cookies.bake(extra_context={'project_slug': 'dummy-project', 'default_docker_registry':'test.test.com'})
+def _get_cookiecutter_config() -> Dict:
+    cookiecutter_config_file = current_dir / "../cookiecutter.json"
+    with cookiecutter_config_file.open() as fp:
+        return json.load(fp)
+
+
+flavors = _get_cookiecutter_config()["docker_base"]
+
+
+@pytest.fixture(params=flavors)
+def baked_project(cookies, request):
+    return cookies.bake(extra_context={'project_slug': 'dummy-project', 'default_docker_registry': 'test.test.com', 'docker_base': request.param})
+
 
 commands = (
-        "ls -la .",
-        "make help",
-        "make devenv",
-        "make devenv build",
-        "make devenv build-devel",
-        "make info-build",
-        "make devenv tests",
-    )
+    "ls -la .",
+    "make help",
+    "make devenv",
+    "make devenv build",
+    "make devenv build-devel",
+    "make info-build",
+    "make devenv tests",
+)
+
 
 @pytest.mark.parametrize("command", commands)
 def test_run_tests(baked_project, command: str):
     working_dir = Path(baked_project.project)
-    assert subprocess.run(command.split(), cwd=working_dir, check=True).returncode == 0
+    assert subprocess.run(command.split(), cwd=working_dir,
+                          check=True).returncode == 0
