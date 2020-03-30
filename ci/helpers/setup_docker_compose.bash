8936ce5141a1c8b84f3eaae2702d212f2663e440
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-03-30 16:23:06 +0200

    docker-compose is not needed

--- ci/helpers/setup_docker_compose.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -euo pipefail
-IFS=$'\n\t'
-
-curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
